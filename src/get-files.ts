b1c348f49dabb1489ff0bb1b19304183899b5e6a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-05 17:04:35 +0200

    docs: update comments and doc comments

--- src/get-files.ts
+++ src/get-files.ts
@@ -1,21 +1,66 @@
 /* `EventFile` represents a file returned from an event. */
 export type EventFile = {
-	/* The `File` returned from a files input or a drop event. */
+	/* The `File` returned from a file input or a drop event. */
 	file: File;
 
 	/* The `FileSystemFileEntry` associated to a `File`.
 	This entry is only available when a directory is dragged and dropped
-	and its files are extracted; it can be used to retrieve the `entry.fullPath`
+	and its files are read; it can be used to retrieve the `entry.fullPath`
 	of a file to reconstruct the directory file structure.
 	This entry may not be available in all browsers. */
 	entry?: FileSystemFileEntry;
 };
 
 /**
-`getFiles` returns the list of `File`s from a `change` or `drop` Event.
+`getFiles` returns the list of files from a `change` or `drop` Event.
+
+@remarks
+`getFiles` supports only the `change` event type on file inputs and the `drop` event type (`DragEvent`).
+Calling `getFiles` with other types of events will return an empty list.
+
+For directory drop events, `getFiles` relies on the `webkitGetAsEntry` API;
+if this API is unavailable in the browser, `getFiles` will return an empty list.
+
+@param event - The `change` or `drop` `Event` for which files should be retrieved.
+@returns A list of `EventFile`s that contain the `File`s retrieved from an `Event`.
+
+@example
+```typescript
+import { getFiles } from "event-to-files";
+
+// For an `<input type="file" />`.
+const fileInput = document.getElementById("file-input");
+fileInput.addEventListener("change", async (event) => {
+	event.preventDefault();
+	const files = await getFiles(event);
+	for (const { file } of files) {
+		console.log(file.name);
+	}
+});
+
+// For a drag and drop event.
+const dropZone = document.getElementById("drop-zone");
+dropZone.addEventListener("dragover", (event) => {
+	// Cancel the `dragover` event to let the `drop` event fire later.
+	event.preventDefault();
+});
+dropZone.addEventListener("drop", async (event) => {
+	event.preventDefault();
+	const files = await getFiles(event);
+	for (const { file, entry } of files) {
+		console.log(file.name);
+
+		// If a directory was dragged and dropped,
+		// we can get the file's full path in the directory.
+		if (entry?.fullPath) {
+			console.log(entry.fullPath);
+		}
+	}
+});
+```
 */
 export let getFiles = async (event: Event): Promise<EventFile[]> => {
-	// Get the list of files from the `<input type="files" />` `change` event.
+	// Get the list of files from the `<input type="file" />` `change` event.
 	if (event.type === "change") {
 		return [...((event.target as HTMLInputElement | null)?.files ?? [])].map((file) => ({ file }));
 	}
@@ -24,7 +69,7 @@ export let getFiles = async (event: Event): Promise<EventFile[]> => {
 	if (event.type === "drop") {
 		return (
 			await Promise.all(
-				// Get the data transfer items of kind `file`.
+				// Get the data transfer items of kind `file` and read the files.
 				[...((event as DragEvent).dataTransfer?.items ?? [])]
 					.filter(({ kind }) => kind === "file")
 					.map(itemToFiles),
