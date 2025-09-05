709ebb843fed5d93070e008f4b160e65ae5e2a7a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-05 17:20:41 +0200

    docs: add package documentation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,41 @@
 /**
-@example
+This package exports an async function `getFiles` that returns
+the list of `File`s retrieved from a `change` event on file inputs
+or from a `drop` event when dragging and dropping files.
 
+@example
 ```typescript
 import { getFiles } from "event-to-files";
+
+// For an `<input type="file" />`.
+const fileInput = document.getElementById("file-input");
+fileInput.addEventListener("change", async (event) => {
+	event.preventDefault();
+	const files = await getFiles(event);
+	for (const { file } of files) {
+		console.log(file.name);
+	}
+});
+
+// For a drag and drop event.
+const dropZone = document.getElementById("drop-zone");
+dropZone.addEventListener("dragover", (event) => {
+	// Cancel the `dragover` event to let the `drop` event fire later.
+	event.preventDefault();
+});
+dropZone.addEventListener("drop", async (event) => {
+	event.preventDefault();
+	const files = await getFiles(event);
+	for (const { file, entry } of files) {
+		console.log(file.name);
+
+		// If a directory was dragged and dropped,
+		// we can get the file's full path in the directory.
+		if (entry?.fullPath) {
+			console.log(entry.fullPath);
+		}
+	}
+});
 ```
 
 @packageDocumentation
